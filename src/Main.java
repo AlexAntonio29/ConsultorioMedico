25842db4027896755589f755a13d319914a64bc9
Jeaustin <164907568+JeaustinAvik@users.noreply.github.com>
2025-04-12 10:58:49 -0600

    Actualizacion

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,17 @@
-
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-
+;
 public class Main extends Application {
 
 
 
     public void start(Stage primaryStage) {
 
-       FormularioPaciente formulario = new FormularioPaciente();
+    /*    FormularioPaciente formulario = new FormularioPaciente();
 
-       Scene contenidoVentana = new Scene(formulario.getRoot(),800,800);
+        Scene contenidoVentana = new Scene(formulario.getRoot(),800,800);
 
         primaryStage.setTitle("Consultorio");
 
@@ -21,13 +20,12 @@ public class Main extends Application {
         primaryStage.show();
 
 
-
+*/
     }
 
 
 
     public static void main(String[] args) {
-        launch(args);  // Llamada para iniciar la aplicación JavaFX
+      //  launch(args);  // Llamada para iniciar la aplicación JavaFX
     }
 }
-
