f27afcbfceccfd4e82e09249e4f53f976f7c2580
AlexAntonio29 <alexantonioangel29@gmail.com>
2025-04-12 16:54:46 -0600

    Update arreglo de Proyecto

--- src/Main.java
+++ src/Main.java
@@ -4,23 +4,29 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+public class Main extends Application  {
+
     @Override
     public void start(Stage primaryStage) throws Exception {
-       // Cargar el archivo FXML
-        Parent root = FXMLLoader.load(getClass().getResource("panel_control.fxml"));
+        // Crear una instancia de FormularioPaciente si necesitas usarlo
+        // FormularioPaciente formulario = new FormularioPaciente();
 
-        // Crear la escena y asignarla al stage
-        primaryStage.setTitle("Mi App JavaFX");
-        primaryStage.setScene(new Scene(root, 800, 600));
+        // Crear una escena y asignar un tamaño básico
+        Scene scene = new Scene(new VBox(), 400, 400); // Ajusta el tamaño de la ventana
+
+        // Configurar el título de la ventana
+        primaryStage.setTitle("Formulario de Paciente");
+
+        // Establecer la escena en el escenario (ventana)
+        primaryStage.setScene(scene);
+
+        // Mostrar el escenario
         primaryStage.show();
-
-
-
-
     }
+
 
     public static void main(String[] args) {
         launch(args);
