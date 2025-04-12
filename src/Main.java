e74b331cacd9b377a960681026dc445e6947657e
AlexAntonio29 <alexantonioangel29@gmail.com>
2025-04-12 10:54:32 -0600

    agregado panel

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,29 @@
 
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+       // Cargar el archivo FXML
+        Parent root = FXMLLoader.load(getClass().getResource("panel_control.fxml"));
 
-
-
-    public void start(Stage primaryStage) {
-
-       FormularioPaciente formulario = new FormularioPaciente();
-
-       Scene contenidoVentana = new Scene(formulario.getRoot(),800,800);
-
-        primaryStage.setTitle("Consultorio");
-
-        primaryStage.setScene(contenidoVentana);
-
+        // Crear la escena y asignarla al stage
+        primaryStage.setTitle("Mi App JavaFX");
+        primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
+
 
 
 
     }
 
-
-
     public static void main(String[] args) {
-        launch(args);  // Llamada para iniciar la aplicación JavaFX
+        launch(args);
     }
 }
 
