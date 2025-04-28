0ea8e592916ce57be6e1fee0a57fd7dd9230f2eb
Jeaustin <164907568+JeaustinAvik@users.noreply.github.com>
2025-04-28 14:55:51 -0600

    error generado

---
+++ src/iniciar.java
@@ -0,0 +1,31 @@
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class iniciar extends Application {
+
+
+
+    public void start(Stage primaryStage) {
+
+       //FormularioPaciente formulario = new FormularioPaciente();
+
+    //   Scene contenidoVentana = new Scene(formulario.getRoot(),800,800);
+
+        primaryStage.setTitle("Consultorio");
+
+      //  primaryStage.setScene(contenidoVentana);
+
+        primaryStage.show();
+
+
+
+    }
+
+
+
+    public static void main(String[] args) {
+        launch(args);  // Llamada para iniciar la aplicación JavaFX
+    }
+}
+
